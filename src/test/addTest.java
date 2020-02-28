643266887ec5e36e26f9c78fa1328379e1c20e88
hp <hp@DESKTOP-0AIAPDE.mynet>
2020-02-28 14:25:26 +0200

    secound

--- src/test/addTest.java
+++ src/test/addTest.java
@@ -2,10 +2,10 @@ package test;
 import calc.calculater;
 import static org.junit.Assert.*;
 
-import org.junit.After;
+/*import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.BeforeClass;
+import org.junit.BeforeClass;*/
 import org.junit.Test;
 
 import calc.calculater;
